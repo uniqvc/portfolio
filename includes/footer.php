6662dd80526a7b3c24e7e10b5de8246ea566bd08
Erick Sahara <erickmsk8@hotmail.com>
2021-01-31 12:39:24 -0300

    adicionando botoes Whatsapp e Solicite um orçamento

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 	<h2 class="h2 text-uppercase text-inverse">Contato</h2>
 	<div class="row">
 		<div class="footer-mensagem col-sm-6 col-sm-pull-2 right">
-			<h3 class="h2 text-inverse text-uppercase">Deixe um mensagem</h3>
+			<h3 class="h2 text-inverse text-uppercase">Receba um orçamento</h3>
 			<form id="formContato" name="formContato" class="form-contato" action="#" method="post">
 				<div class="form-group"> 
 					<label for="email" class="sr-only">E-mail</label> 
@@ -44,3 +44,83 @@
 		</div>
 	</div>
 </footer>
+
+<!-- Form Uniq 2.0 -->
+
+<!-- <footer id="contato" class="footer container-padding" data-header-white>
+	<h2 class="h2 text-uppercase text-inverse">Contato</h2>
+	<div class="row">
+		<div class="footer-mensagem col-xs-12 col-sm-6 col-sm-pull-2 right">
+			<h3 class="h2 text-inverse text-uppercase">Receba um orçamento</h3>
+			<form id="formContato" name="formContato" class="form-contato" action="#" method="post">
+				<div class="row">
+					<div class="form-group col-sm-6"> 
+						<label for="nome" class="sr-only">Nome/Nome da empresa</label> 
+						<input type="nome" name="email" class="form-control form-control input-lg form-control-inverse no-focus" id="email" placeholder="Nome/Nome da empresa" required> 
+					</div>
+					<div class="form-group col-sm-6"> 
+						<label for="email" class="sr-only">E-mail</label> 
+						<input type="email" name="email" class="form-control form-control input-lg form-control-inverse no-focus" id="email" placeholder="E-mail" required> 
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col-sm-6"> 
+						<label for="whatsapp" class="sr-only">Whatsapp</label> 
+						<input type="tel" name="whatsapp" class="form-control form-control input-lg form-control-inverse no-focus" id="email" placeholder="Whatsapp" required> 
+					</div>
+					<div class="form-group col-sm-6"> 
+						<label for="servicoInteresse" class="sr-only">Serviço de interesse</label> 
+						<select name="servicoInteresse" id="servicoInteresse" class="form-control form-control input-lg form-control-inverse no-focus input-servico-interesse" onchange="this.value !== '' ? document.getElementById('msg').classList.remove('hide') : document.getElementById('msg').classList.add('hide')" required>
+							<option value="">Serviço de interesse</option>	
+							<option value="UX Design">UX Design</option>
+							<option value="Criacao de website">Criação de website</option>
+							<option value="Criaçao de logo">Criação de logo</option>
+							<option value="Outros">Outros</option>
+						</select>
+					</div>
+				</div>
+				<div class="form-group"> 
+					<label for="msg" class="sr-only">Mensagem</label>
+					<textarea name="mensagem" id="msg" cols="30" rows="10" class="form-control form-control-inverse input-lg no-focus hide" placeholder="Conte-nos um pouco sobre o seu projeto e o que você precisa"></textarea>
+				</div>
+				<div class="form-group"> 
+					<button class="btn btn-inverse btn-lg" type="submit">Enviar mensagem</button>
+				</div>
+			</form>
+		</div>
+		<div class="col-xs-12 col-sm-4">
+			<div class="footer-telefone">		
+				<h3 class="h2 text-inverse text-uppercase">Telefone</h3>
+				<p class="footer-phone text-inverse"><a class="link-tel" href="tel:11990115724">11 99011-5724</a></p>
+			</div>
+			<div class="footer-endereco">
+				<h3 class="h2 text-inverse text-uppercase">Endereço</h3>
+				<p class="footer-phone text-inverse">Rua Maquerobi, 212 ap 6 - Saúde <br> São Paulo - SP</p>
+			</div>
+			<div class="footer-socialmedia">
+				<a href="https://www.instagram.com/uniq_digital/" target="_blank">
+				<svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M15 6.8125C11 6.8125 7.8125 10.0625 7.8125 14C7.8125 18 11 21.1875 15 21.1875C18.9375 21.1875 22.1875 18 22.1875 14C22.1875 10.0625 18.9375 6.8125 15 6.8125ZM15 18.6875C12.4375 18.6875 10.3125 16.625 10.3125 14C10.3125 11.4375 12.375 9.375 15 9.375C17.5625 9.375 19.625 11.4375 19.625 14C19.625 16.625 17.5625 18.6875 15 18.6875ZM24.125 6.5625C24.125 5.625 23.375 4.875 22.4375 4.875C21.5 4.875 20.75 5.625 20.75 6.5625C20.75 7.5 21.5 8.25 22.4375 8.25C23.375 8.25 24.125 7.5 24.125 6.5625ZM28.875 8.25C28.75 6 28.25 4 26.625 2.375C25 0.75 23 0.25 20.75 0.125C18.4375 0 11.5 0 9.1875 0.125C6.9375 0.25 5 0.75 3.3125 2.375C1.6875 4 1.1875 6 1.0625 8.25C0.9375 10.5625 0.9375 17.5 1.0625 19.8125C1.1875 22.0625 1.6875 24 3.3125 25.6875C5 27.3125 6.9375 27.8125 9.1875 27.9375C11.5 28.0625 18.4375 28.0625 20.75 27.9375C23 27.8125 25 27.3125 26.625 25.6875C28.25 24 28.75 22.0625 28.875 19.8125C29 17.5 29 10.5625 28.875 8.25ZM25.875 22.25C25.4375 23.5 24.4375 24.4375 23.25 24.9375C21.375 25.6875 17 25.5 15 25.5C12.9375 25.5 8.5625 25.6875 6.75 24.9375C5.5 24.4375 4.5625 23.5 4.0625 22.25C3.3125 20.4375 3.5 16.0625 3.5 14C3.5 12 3.3125 7.625 4.0625 5.75C4.5625 4.5625 5.5 3.625 6.75 3.125C8.5625 2.375 12.9375 2.5625 15 2.5625C17 2.5625 21.375 2.375 23.25 3.125C24.4375 3.5625 25.375 4.5625 25.875 5.75C26.625 7.625 26.4375 12 26.4375 14C26.4375 16.0625 26.625 20.4375 25.875 22.25Z" fill="#B4B4B4"/>
+				</svg>
+				</a>
+				<a href="https://www.facebook.com/uniq.vc" target="_blank">
+				<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M31.5 16C31.5 7.4375 24.5625 0.5 16 0.5C7.4375 0.5 0.5 7.4375 0.5 16C0.5 23.75 6.125 30.1875 13.5625 31.3125V20.5H9.625V16H13.5625V12.625C13.5625 8.75 15.875 6.5625 19.375 6.5625C21.125 6.5625 22.875 6.875 22.875 6.875V10.6875H20.9375C19 10.6875 18.375 11.875 18.375 13.125V16H22.6875L22 20.5H18.375V31.3125C25.8125 30.1875 31.5 23.75 31.5 16Z" fill="#B4B4B4"/>
+				</svg>
+				</a>
+			</div>
+			<div class="footer-direitos-autorais">
+				<small class="text-inverse">© Uniq 2016-2021</small>
+			</div>
+		</div>
+	</div>
+</footer> -->
+
+<!-- Fim Form Uniq 2.0 -->
+
+
+<a class="btn-whatsapp" href="https://wa.me/5511990115724">
+<svg width="30" height="31" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M25.2344 4.75781C22.5117 1.96875 18.793 0.375 14.8086 0.375C6.70703 0.375 0.0664062 7.01562 0.0664062 15.1172C0.0664062 17.7734 0.796875 20.2969 2.05859 22.4883L0 30.125L7.76953 28.1328C9.96094 29.2617 12.3516 29.9258 14.8086 29.9258C22.9766 29.9258 29.75 23.2852 29.75 15.1836C29.75 11.1992 28.0234 7.54688 25.2344 4.75781ZM14.8086 27.4023C12.6172 27.4023 10.4922 26.8047 8.56641 25.6758L8.16797 25.4102L3.51953 26.6719L4.78125 22.1562L4.44922 21.6914C3.25391 19.6992 2.58984 17.4414 2.58984 15.1172C2.58984 8.41016 8.10156 2.89844 14.875 2.89844C18.1289 2.89844 21.1836 4.16016 23.5078 6.48438C25.832 8.80859 27.2266 11.8633 27.2266 15.1836C27.2266 21.8906 21.582 27.4023 14.8086 27.4023ZM21.582 18.2383C21.1836 18.0391 19.3906 17.1758 19.0586 17.043C18.7266 16.9102 18.4609 16.8438 18.1953 17.2422C17.9961 17.5742 17.2656 18.4375 17.0664 18.7031C16.8008 18.9023 16.6016 18.9688 16.2695 18.7695C14.0781 17.707 12.6836 16.8438 11.2227 14.3867C10.8242 13.7227 11.6211 13.7891 12.2852 12.3945C12.418 12.1289 12.3516 11.9297 12.2852 11.7305C12.2188 11.5312 11.4219 9.73828 11.1562 9.00781C10.8242 8.27734 10.5586 8.34375 10.293 8.34375C10.0938 8.34375 9.82812 8.34375 9.62891 8.34375C9.36328 8.34375 8.96484 8.41016 8.63281 8.80859C8.30078 9.20703 7.37109 10.0703 7.37109 11.8633C7.37109 13.7227 8.63281 15.4492 8.83203 15.7148C9.03125 15.9141 11.4219 19.6328 15.1406 21.2266C17.4648 22.2891 18.3945 22.3555 19.5898 22.1562C20.2539 22.0898 21.7148 21.293 22.0469 20.4297C22.3789 19.5664 22.3789 18.8359 22.2461 18.7031C22.1797 18.5039 21.9141 18.4375 21.582 18.2383Z" fill="white"/>
+</svg>
+</a>
